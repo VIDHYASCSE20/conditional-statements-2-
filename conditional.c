bea15424c9a8f5946e7fdd98f299e6c9b374a67c
VIDHYASCSE20 <93870370+VIDHYASCSE20@users.noreply.github.com>
2022-04-28 23:49:32 +0530

    conditional.c

---
+++ conditional.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+         int a;
+         printf("1,Pizza Rs.239 2.Burger Rs.129 3.pasta Rs.179 4.french fries Rs.99 5.Sandwich Rs.149");
+         printf("Enter the menu:");
+         scanf("%d",&a);
+         switch(a)
+         {
+           case 1 :
+                    printf("pizza Rs.239");
+                    break;
+           case 2 :
+                    print("Burger Rs.129");
+                    break;
+           case 3 :
+                    print("pasta Rs.179");
+                    break;
+           case 4 :
+                     printf("French fries Rs.99");
+                     break;
+           case 5 :
+                     printf(Sandwich Rs.149");
+                     break;
+                     default;
+                     printf("the menu is not ordered");
+   }                
+                                                                                  
+                            
